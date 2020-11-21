6b5f1ba4962871dac2ecfb55a85023eed119c087
Serhii Datsii <sdatsiy@morebis.pl>
2020-11-21 17:46:23 +0200

    cocktails list

--- projects/Server/src/Cocktails/cocktails.api.ts
+++ projects/Server/src/Cocktails/cocktails.api.ts
@@ -1,4 +1,6 @@
 import { Api } from '../Core';
+import { IGetManyRequest, IResponse } from '../Core/interfaces';
+import { dbConnection } from '../Database/connections';
 import { Cocktail } from '../Database/entities/Cocktail';
 
 export class CocktailsApi extends Api {
@@ -8,5 +10,16 @@ export class CocktailsApi extends Api {
     super({ baseUrl: 'cocktails' });
 
     this.entity = Cocktail;
+  }
+
+  protected async getMany(request: IGetManyRequest, response: IResponse): Promise<void> {
+    const res = await dbConnection
+      .getRepository(this.entity)
+      .find({
+        relations: ['components', 'components.drink']
+      })
+    ;
+
+    response.json(res);
   }
 }
