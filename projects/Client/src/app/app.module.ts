b3bb4157a9943d2be078ea91125d2581c58207a8
ylytvyn <julia.lytvyn.wonder@gmail.com>
2020-11-21 17:07:51 +0200

    login

--- projects/Client/src/app/app.module.ts
+++ projects/Client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ApiService } from './api.service';
-import { AppRoutingModule } from './app-routing,module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
