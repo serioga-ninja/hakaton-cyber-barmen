aa8bd8a166e421d35bc58e2307db6b73928e2774
Andrii Polishchuk <apolishchuk@morebis.pl>
2020-11-21 12:37:09 +0200

    Add pages login and cocktails

--- projects/Client/src/app/app.module.ts
+++ projects/Client/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing,module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CocktailsComponent } from './cocktails/cocktails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CocktailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
