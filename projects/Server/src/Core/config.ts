d65ab3f1628cf1017adc513389970fef3ca64f89
Serhii Datsii <sdatsiy@morebis.pl>
2020-11-26 15:41:07 +0200

    set default value to DB type

--- projects/Server/src/Core/config.ts
+++ projects/Server/src/Core/config.ts
@@ -6,7 +6,7 @@ const config = {
   DEVICE_URL: process.env.DEVICE_URL || 'http://localhost:3001',
 
   DB_PORT: process.env.DB_PORT,
-  DB_TYPE: process.env.DB_TYPE as 'sqlite3' | 'postgres',
+  DB_TYPE: process.env.DB_TYPE as 'sqlite3' | 'postgres' || 'sqlite3',
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
